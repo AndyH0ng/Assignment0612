f53f0012d37a914c29a0fd97665702f5f8b95349
Andy Hong <chili.tomat0@icloud.com>
2024-04-23 15:36:06 +0900

    0423

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+    createCanvas(400, 400);
+    let time = [0, 0, 0, 0];
+    // scene 총 6개 + Ending Cut
+    const MAIN = 0;
+    const TEAM1 = 1;
+    const TEAM2 = 2;
+    const TEAM3_1 = 3;
+    const TEAM3_2 = 4;
+    const TEAM3_3 = 5;
+    const TEAM3_4 = 6;
+    const ENDINGCREDIT = 7;
+    let currentScene = 0;
+}
+ㄴ
+function draw() {
+
+}
