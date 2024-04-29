9bdaec7adfe777c1943b85b38ee624eba2fb2c9a
Andy Hong <chili.tomat0@icloud.com>
2024-04-29 11:11:56 +0900

    지정한 시간이 지나면 다음 씬으로 넘어가도록 구현

--- sketch.js
+++ sketch.js
@@ -1,23 +1,66 @@
 let bgColor = 0;
 let isSort = true;
+let totalTime;
+let currentScene;
+let time = [1, 1, 1, 1, 1, 1, 1, 1];
+// scene 총 6개 + Ending Cut
+const M = 0;
+const TEAM1 = 1;
+const TEAM2 = 2;
+const TEAM3_1 = 3;
+const TEAM3_2 = 4;
+const TEAM3_3 = 5;
+const TEAM3_4 = 6;
+const ENDINGCREDIT = 7;
+
+const devMode = true;
+
 function setup() {
     createCanvas(400, 400);
-    let time = [0, 0, 0, 0];
-    // scene 총 6개 + Ending Cut
-    const MAIN = 0;
-    const TEAM1 = 1;
-    const TEAM2 = 2;
-    const TEAM3_1 = 3;
-    const TEAM3_2 = 4;
-    const TEAM3_3 = 5;
-    const TEAM3_4 = 6;
-    const ENDINGCREDIT = 7;
-    let currentScene = 0;
+    currentScene = 0;
+    totalTime = 0;
 }
+
 function draw() {
     background(bgColor);
     if (isSort) bgColor++;
     else bgColor--;
     if (bgColor == 255) isSort = false;
     else if (bgColor == 0) isSort = true;
+    trackTime(millis());
+    switch (currentScene) {
+        case M:
+            break;
+        case TEAM1:
+            team01();
+            break;
+        case TEAM2:
+            team02();
+            break;
+        case TEAM3_1:
+            ending01();
+            break;
+        case TEAM3_2:
+            ending02();
+            break;
+        case TEAM3_3:
+            ending03();
+            break;
+        case TEAM3_4:
+            ending04();
+            break;
+        case ENDINGCREDIT:
+            break;
+    }
+
+}
+
+function trackTime(args) {
+    if (devMode) print(currentScene);
+    if (args - totalTime >= 1000 * time[currentScene]) {
+        if (currentScene != time.length - 1) {
+            totalTime += 1000 * time[currentScene];
+            currentScene++;
+        }
+    }
 }
